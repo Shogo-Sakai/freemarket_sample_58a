1d2140412174bf120058f4c26d952d6ad22eeb81
kazuki mokuo <sp9t5ns9@icloud.com>
2019-09-10 21:42:17 +0900

    Add signup and signin view
    signup時のフロントのビューを実装した。

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,31 @@
+class SignupController < ApplicationController
+
+  def signup
+    render layout: 'form_layout'
+  end
+
+  def first
+    render layout: 'signup_layout'
+  end
+
+  def second
+
+  end
+
+  def third
+
+  end
+
+  def foruth
+
+  end
+
+  def fifth
+
+  end
+
+  def signin
+    render layout: 'form_layout'
+  end
+
+end
