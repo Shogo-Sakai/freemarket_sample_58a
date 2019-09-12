61ab255c4d46da70711d2a2ef0db8072a9954b17
shogosakai <shogosakai73@gmail.com>
2019-09-12 18:02:22 +0900

    made users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'sakais/index'
-  get 'products/index'
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources 'products'
   root 'takasus#index'
 end
