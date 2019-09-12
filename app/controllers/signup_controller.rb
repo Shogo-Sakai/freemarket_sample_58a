cf7a3bb2100f4482a2eb67b5b25b7ade676f8a3e
kazuki mokuo <sp9t5ns9@icloud.com>
2019-09-12 18:34:29 +0900

    delete signup_layouts and replace header and footer
    ヘッダー・フッターを部分テンプレート化し、呼び出すレイアウトファイルを１つにした。
    その後、コントローラでのレイアウト呼び出し記述を変更した。
    
    ページ毎に動的にタイトルを変更できるようにコードを記載した。

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,31 +1,25 @@
 class SignupController < ApplicationController
-
+  layout 'form_layout'
+  
   def signup
-    render layout: 'form_layout'
   end
 
   def first
-    render layout: 'layout_1'
   end
 
   def second
-    render layout: 'layout_2'
   end
 
   def third
-    render layout: 'layout_3'
   end
 
   def fourth
-    render layout: 'layout_4'
   end
 
   def fifth
-    render layout: 'layout_5'
   end
 
   def signin
-    render layout: 'form_layout'
   end
 
 end
