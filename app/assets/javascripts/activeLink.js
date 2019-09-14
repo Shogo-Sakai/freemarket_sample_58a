07d7397ae190359ba136a0b8d31f2664ea55b1da
shogosakai <shogosakai73@gmail.com>
2019-09-14 11:36:18 +0900

    made navigation menu responsible.

--- app/assets/javascripts/activeLink.js
+++ app/assets/javascripts/activeLink.js
@@ -1,10 +1,9 @@
-$(window).bind("load", function(){
-  $("a").each(function(){
-    var activeLink = $(this).attr("href")
-    console.log(activeLink)
-    if(document.URL.match(activeLink)){
-      $(this).addClass('active')
-      // return false;
+$(window).bind("turbolinks:load", function(){
+  $(".link").each(function(){
+    var link = $(this).attr("href")
+    if(document.URL.match(link)){
+        $(this).addClass('active')
+      return false;
     };
   });
-})
+});
