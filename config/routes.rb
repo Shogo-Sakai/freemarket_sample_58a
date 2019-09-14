f6e846d3c199d3e5b59bc05a41609b04b160b39a
shogosakai <shogosakai73@gmail.com>
2019-09-14 22:26:13 +0900

    made identification page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
+  get 'users/edit'
+  resources 'users'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'mercaris#index'
   resources 'products'
