deda9181c9be4cb0b962cf22c7a69250d39abbc3
shogosakai <shogosakai73@gmail.com>
2019-09-15 12:54:41 +0900

    updated user routings and controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,29 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  resources 'users'
+
+  
+  # ユーザー
+  resources 'users', except: [:show, :edit] do
+    collection do
+      get 'credit_register'
+      get 'show'
+      get 'edit'
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'mercaris#index'
-  resources 'products'
   root 'products#new'
-  get 'signup/signup' => 'signup#signup'
-  get 'signup/signin' => 'signup#signin'
-  get 'signup/1' => 'signup#first'
-  get 'signup/2' => 'signup#second'
-  get 'signup/3' => 'signup#third'
-  get 'signup/4' => 'signup#fourth'
-  get 'signup/5' => 'signup#fifth'
-  get 'users/credit_register' => 'users#credit_register'
+
+  # signup
+  resources 'signup' do
+    collection do
+      get 'signup'
+      get 'signin'
+      get 'first'
+      get 'second'
+      get 'third'
+      get 'fourth'
+      get 'fifth'
+    end
+  end
 end
