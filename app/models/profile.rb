a6f77952f0813bca6b159f5c5e92c11f52d1d1b6
kazuki mokuo <sp9t5ns9@icloud.com>
2019-09-16 18:32:50 +0900

    add_sms_confirmation_function
    バリデーションの追加およびsms認証機能を導入した。
    それに伴ってgem "twillio"を導入し、コントローラにsms認証処理を書き足した。

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,19 +1,21 @@
 class Profile < ApplicationRecord
   belongs_to :user
-
+  POSTAL_CODE_VALID = /\A\d{3}-\d{4}\z/i
+  
   validates :birthyear, presence: true
   validates :birthmonth, presence: true
   validates :birthday, presence: true
-  validates :family_name, presence: true
-  validates :personal_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :personal_name_kana, presence: true
-  validates :postal_code, presence: true
-  validates :prefecture, presence: true
-  validates :city, presence: true
-  validates :adress, presence: true
-  validates :post_family_name,presence: true
-  validates :post_personal_name,presence: true
-  validates :post_family_name_kana,presence: true
-  validates  :post_personal_name_kana,presence: true
+  validates :family_name, presence: true, length: {maximum: 35}
+  validates :personal_name, presence: true, length: {maximum: 35}
+  validates :family_name_kana, presence: true, length: {maximum: 35}
+  validates :personal_name_kana, presence: true, length: {maximum: 35}
+  validates :postal_code, presence: true, length: {maximum: 100},format: { with: POSTAL_CODE_VALID }
+  validates :prefecture, presence: true, length: {maximum: 100}
+  validates :city, presence: true, length: {maximum: 50}
+  validates :adress, presence: true, length: {maximum: 100}
+  validates :post_family_name,presence: true, length: {maximum: 35}
+  validates :post_personal_name,presence: true, length: {maximum: 35}
+  validates :post_family_name_kana,presence: true, length: {maximum: 35}
+  validates :post_personal_name_kana,presence: true, length: {maximum: 35}
+  validates :tel, length: {maximum: 100}
 end
