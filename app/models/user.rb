6053ba110232e54c93c121ac9270c902dc8a2ecf
kazuki mokuo <sp9t5ns9@icloud.com>
2019-09-18 20:14:41 +0900

    add_signin_function
    ログイン機能および新規登録後のログイン処理を実装した。
    また、ログアウトボタンを押した際のログアウト機能を実装した。
    ルーティングを変更し、signinコントローラを作成してsignin部分を切り離した。
    formsディレクトリを作り、formobjectを使用したSigninクラスを作成した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :profile
   has_one :creditcard
-  
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   PASSWORD_VALIDATION = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i
-  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
-  validates :password,                presence: true, length: {minimum: 6, maximum: 128},format: { with: PASSWORD_VALIDATION }
-  validates :password_confirmation,   presence: true, length: {minimum: 6, maximum: 128}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: {minimum: 6, maximum: 128},format: { with: PASSWORD_VALIDATION }
+  validates :password_confirmation, presence: true, length: {minimum: 6, maximum: 128}
   validates :nickname, presence: true,length: {maximum: 20}
 end
