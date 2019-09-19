a8ba58660685e4b7887e49f735b021123a0dbe50
shogosakai <shogosakai73@gmail.com>
2019-09-19 17:40:41 +0900

    made active hash of freshstatus, deliver columns.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :size
   belongs_to :brand
   has_many :images
   has_one :price
   has_one :delivery
+  has_one_active_hash :product_freshness
   
   # カテゴリ関係
   has_one :category_index
