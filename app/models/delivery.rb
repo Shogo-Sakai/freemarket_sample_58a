7c7872a98091025b6da1056993d22ff02f162b0d
shogosakai <shogosakai73@gmail.com>
2019-09-19 17:40:41 +0900

    made active hash of freshstatus, deliver columns.

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,7 @@
 class Delivery < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :product
+  has_one_active_hash :deliver_fee
+  has_one_active_hash :deliver_how
+  has_one_active_hash :deliver_day
 end
