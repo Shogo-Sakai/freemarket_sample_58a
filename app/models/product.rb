4cd0a317bfb947573a422cef371e44c9883b6c69
shogosakai <shogosakai73@gmail.com>
2019-09-21 21:49:29 +0900

    made validation and fixed layout of error view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,17 @@
 class Product < ApplicationRecord
-  belongs_to :user,  optional:true
-  belongs_to :brand, optional:true
-  has_many :product_images,  dependent: :destroy
+  validates :name,           presence: true, length:    {maximum: 40}
+  validates :text,           presence: true, length:    {maximum: 1000}
+  validates :category_index, presence:true,  exclusion: {in: %w(--- nil ) }
+  validates :fresh_status ,  presence: true, exclusion: {in: %w(---) }
+
+  # アソシエーション
+  belongs_to :user,                             optional:true
+  belongs_to :brand,                            optional:true
+  has_many :product_images,                     dependent: :destroy
   accepts_nested_attributes_for :product_images
-  has_one :price, dependent: :destroy
+  has_one :price,                               dependent: :destroy
   accepts_nested_attributes_for :price
-  has_one :delivery, dependent: :destroy
+  has_one :delivery,                            dependent: :destroy
   accepts_nested_attributes_for :delivery
   
   # カテゴリ関係
