98ad2effef65f0f8a441e4eb379e1373d681aa09
shogosakai <shogosakai73@gmail.com>
2019-09-22 12:43:11 +0900

    rebased from mr.takasu master

--- app/models/delivery.rb
+++
@@ -1,8 +0,0 @@
-class Delivery < ApplicationRecord
-  validates :deliver_way, exclusion: {in: %w(---)}
-  validates :fee_person,  exclusion: {in: %w(---)}
-  validates :from_area,   exclusion: {in: %w(---)}
-  validates :sending_day, exclusion: {in: %w(---)}
-
-  belongs_to :product, optional: true
-end
