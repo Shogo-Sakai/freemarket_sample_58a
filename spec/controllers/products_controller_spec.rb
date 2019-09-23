56a4d79da2c8b45ffd0106fc001f7698fdc178a8
TuBuKe <tubuke0657@gmail.com>
2019-09-23 20:17:26 +0900

    create_test_code
    商品詳細コードのテストコードを作成。

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -12,15 +12,22 @@ describe ProductsController do
     it "renders the :show template" do
       category = create(:category_index)
       product = create(:product)
-      get :show, params:{id: product.id}
+      get :show, params:{id:product.id}
+    end
+
+    it "assigns the requested product to @product" do
+      category = create(:category_index)
+      product = create(:product)
+      get :show, params: {id:product.id}
+      expect(assigns(:product)).to eq product
     end
   end
 
   describe 'POST #create' do
     it 'redirect to error page if error' do
+      category = create(:category_index)
       user = create(:user)
       sign_in(user)
-      category = create(:category_index)
       post :create, params:{ product:{title:"test product", text:"test text", category_index_id: 1, fresh_status:"新品、未使用", deliver_way:"ゆうパック", deliver_person:"送料込み(出品者負担)", from_area:"三重県", deliver_leadtime:"3-4日で発送", price:"3000"}, product_image: attributes_for(:product_image, {image:["test.jpeg", "test2.jpeg"]})}
       expect(response).to redirect_to("http://test.host/")
     end
@@ -28,9 +35,9 @@ describe ProductsController do
 
   describe 'POST #create' do
     it 'redirect to root page if created' do
+      category = create(:category_index)
       user = create(:user)
       sign_in(user)
-      category = create(:category_index)
       post :create, params:{ product:{title:"test product", text:"test text", category_index_id: 1, fresh_status:"新品、未使用", deliver_way:"ゆうパック", deliver_person:"送料込み(出品者負担)", from_area:"三重県", deliver_leadtime:"3-4日で発送", price:"3000"}, product_image: {image:["test.jpeg", "test2.jpeg"]}}
       expect(response).to redirect_to(root_path)
     end
