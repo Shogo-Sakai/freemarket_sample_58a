69bef3c754878d3280a41843f7abe658d2870ef8
kazuki mokuo <sp9t5ns9@icloud.com>
2019-09-24 19:24:33 +0900

    repair category pulldown
    カテゴリのプルダウンのバリデーションエラーが取得できるようにコードを変更した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     @product.user = current_user
 
-    if @product.category_index_id != 0 && params[:product_image].present? && params[:product_image][:image].length <= 10 && @product.save
+    if @product.category_index_id != nil && params[:product_image].present? && params[:product_image][:image].length <= 10 && @product.save
       params[:product_image][:image].each do |image|
         @product.product_images.create(image: image, product_id: @product.id)
       end
@@ -21,7 +21,8 @@ class ProductsController < ApplicationController
       elsif params[:product_image][:image].length > 10
         @product.errors.messages[:image] = ["image is too many"]
       end 
-      @product.errors.messages[:category_index_id] = ["is reserved"] if @product.category_index_id == 0
+      binding.pry
+      @product.errors.messages[:category_index_id] = ["is reserved"] if @product.category_index_id == nil
       render json: {errors: @product.errors.messages},status: 422
     end
   end
