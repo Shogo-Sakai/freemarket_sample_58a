fa6c0f9e462e44e736e39153278462da7f3918eb
shogosakai <shogosakai73@gmail.com>
2019-09-26 13:15:28 +0900

    made indents buetiful in model, assets files.

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,7 +1,7 @@
 class ProductImage < ApplicationRecord
   mount_uploader :image, ImagesUploader
+
+  validates  :image,     presence: true
   
-  validates  :image,   presence: true
-  
-  belongs_to :product, optional: true, dependent: :destroy
+  belongs_to :product,   optional: true, dependent: :destroy
 end
