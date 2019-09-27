eafc7ff7824143a2040857f4b06666e8bc1ce9bc
kazuki mokuo <sp9t5ns9@icloud.com>
2019-09-27 20:16:38 +0900

    created rspec test for products search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,13 +5,14 @@ class SearchController < ApplicationController
 
     if search_params[:keyword].length == 0
       @search_word = "新着商品"
-      @products = Product.order('created_at DESC').limit(30)
+      @products = Product.order('created_at DESC').limit(28)
     else
       @search_word = search_params[:keyword]
       @products = Product.where(["title LIKE(?) OR text LIKE(?) OR fresh_status LIKE(?) OR deliver_way LIKE(?) OR from_area LIKE(?)","%#{search_params[:keyword]}%","%#{search_params[:keyword]}%","%#{search_params[:keyword]}%","%#{search_params[:keyword]}%","%#{search_params[:keyword]}%"])
     end
-    @products = @products.page(params[:page]).per(28)
 
+    @products = @products.page(params[:page]).per(28)
+    
   end
 
   private
