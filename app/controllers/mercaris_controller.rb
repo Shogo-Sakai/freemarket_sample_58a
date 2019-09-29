dda640d1fd2c4d2f43c485a43d00a2445b46adb7
shogosakai <shogosakai73@gmail.com>
2019-09-29 14:02:44 +0900

    updated controllers to view categories in header.

--- app/controllers/mercaris_controller.rb
+++ app/controllers/mercaris_controller.rb
@@ -1,6 +1,12 @@
 class MercarisController < ApplicationController
+  before_action :categories
   def index
     @products        = Product.order('created_at DESC').limit(10)
+  end
+
+  private
+
+  def categories
     @categories      = CategoryIndex.all
     @bigcategories   = Bigcategory.all
     @smallcategories = Smallcategory.all
