fa637e74624714e3070957226cef4170c4f9109c
kazuki mokuo <sp9t5ns9@icloud.com>
2019-10-04 13:44:03 +0900

    created will_paginate.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   before_action :set_categories
-
+  require "kaminari"
+  
   def index
     @form = SearchForm.new(search_params)
 
@@ -12,7 +13,7 @@ class SearchController < ApplicationController
       @products = Product.where(["title LIKE(?) OR text LIKE(?) OR fresh_status LIKE(?) OR deliver_way LIKE(?) OR from_area LIKE(?)","%#{search_params[:keyword]}%","%#{search_params[:keyword]}%","%#{search_params[:keyword]}%","%#{search_params[:keyword]}%","%#{search_params[:keyword]}%"])
     end
 
-    @products = Kaminari.paginate_array(@products).page(params[:page]).per(28)    
+    @products = @products.page(params[:page]).per(28)    
 
   end
 
